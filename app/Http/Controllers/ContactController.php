8862fc9620823f08cb593ea651805c0f4c467265
Hasan Harb <harb.hasn98@gmail.com>
2019-07-30 09:26:26 +0300

    create controleer contact and insert in databases;

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Contact;
+class ContactController extends Controller
+{
+    public function index()
+    {
+        return view('notes.contact');
+    }
+
+    public function store(Request $request)
+    {
+        $contact = new Contact();
+        $contact->name = $request->name;
+        $contact->email = $request->email;
+        $contact->phone = $request->phone;
+        $contact->body = $request->body;
+        $contact->save();
+        return redirect()->back();
+
+
+    }
+}
