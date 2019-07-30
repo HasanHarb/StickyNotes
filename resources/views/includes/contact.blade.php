300771beb62ffe1751f716970f167a565019ba51
Hasan Harb <harb.hasn98@gmail.com>
2019-07-30 09:06:43 +0300

    add style for contact page

--- resources/views/includes/contact.blade.php
+++ resources/views/includes/contact.blade.php
@@ -11,28 +11,23 @@
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-7 col-md-offset-1 col-lg-7 col-lg-offset-1">
                     <div class="section-text">
-                        <p> لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى) ويُستخدم في صناعات
-                            المطابع ودور
-                            النشر. كان لوريم إيبسوم ولايزال المعيار للنص الشكلي </p>
+                        <p>تواصل معنا من خلال البريد الالكترني الخاص بالموقع او يمكنك تعبئة محتويات الرسالة ادناه وارسالها لنا ، في حال واجهتك مشكلة او استفسار</p>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                    <p class="cont-text">لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى) ويُستخدم
-                        في
-                        صناعات المطابع ودور النشر.</p>
                     <address>
-                        <p class=""><i class="fa fa-map-marker"></i> 124 شارع مدينه الدوله</p>
+                        <p class=""><i class="fa fa-map-marker"></i> الخليل - بالقرب من جامعة الخليل</p>
                         <p class=""><i class="fa fa-phone"></i> +1-222-111-2222 </p>
-                        <p class=""><i class="fa fa-envelope"></i> yourmail@domain.com</p>
-                        <p class=""><i class="fa fa-skype"></i> Your Skype Name</p>
+                        <p class=""><i class="fa fa-envelope"></i> info@molahadaty.com </p>
+                        <p class=""><i class="fa fa-skype"></i> @ molahadaty </p>
                         <p class=""><i class="fa fa-clock-o"></i> sun. to thu. 9AM to 5PM </p>
                     </address>
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-7 col-md-offset-1 col-lg-7 col-lg-offset-1">
                     <div class="contact-form">
-                        <form>
+                        <form action="{{ route('contact.store') }}" method="POST">
                             <div class="form-group">
                                 <input type="text" class="form-control" name="name" placeholder="الاسم..." required>
                             </div>
@@ -44,7 +39,7 @@
                                 <input type="text" class="form-control" name="phone" placeholder="الموبيل..." required>
                             </div>
                             <div class="form-group">
-                                <textarea rows="6" placeholder="الرساله..." class="form-control" name="ةثسسشلث"
+                                <textarea rows="6" placeholder="الرساله..." class="form-control" name="body"
                                     required></textarea>
                             </div>
                             <div class="form-group">
